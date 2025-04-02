dcfe4dfba039d74541f9fce7a2205c0f69da68e1
Elijah Zappe <53388831+emzappe@users.noreply.github.com>
2025-04-02 14:36:57 -0600

    Update block.py

--- block.py
+++ block.py
@@ -1,72 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2025 Pi-Radio.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
 import numpy as np
 import urllib.request
 from gnuradio import gr
 from gnuradio import blocks
 
-class blk(gr.sync_block):
+class FR3(gr.sync_block):
     
-    def __init__(self, frequency=1000000, ip_address="0.0.0.0", lo1=0, lo1-=0, lo2=0, lo2-=0):
+    def __init__(self, freq, freq2, gain1, gain2, gain3, gain4, ip_address="0.0.0.0"):
         gr.sync_block.__init__(
             self,
-            name='FR3',   
-            in_sig=None,
-            out_sig=None
+            name='FR3',
+            in_sig=complex,
+            out_sig=complex
         )
-        self.highlo = highlo
-        self.lowlo = lowlo
-        self.rx1gain = rx1gain
-        self.rx2gain = rx2gain
-        self.tx1gain = tx1gain
-        self.tx2gain = tx2gain
+        self.highlo = freq
+        self.lowlo = freq2
+        self.rx1gain = gain1
+        self.rx2gain = gain2
+        self.tx1gain = gain3
+        self.tx2gain = gain4
         self.ip_address = ip_address
 
-     def set_highlo(self, highlo):
+    def set_highlo(self, freq):
         """Set the high lo frequency."""
-        self.highlo = highlo
-        url = f"http://{self.ip_address}:5111/high_lo?freq={self.highlo}"
+        self.freq = freq
+        url = f"http://{self.ip_address}:5111/high_lo?freq={self.freq}"
         response = urllib.request.urlopen(url)
         if response.status == 200:
-            print(f"High LO frequency set to {self.highlo}")
+            print(f"High LO frequency set to {self.freq}")
 
-    def set_lowlo(self, lowlo):
+    def set_lowlo(self, freq2):
         """Set the low lo frequency."""
-        self.lowlo = lowlo
-        url = f"http://{self.ip_address}:5111/low_lo?freq={self.lowlo}"
+        self.freq2 = freq2
+        url = f"http://{self.ip_address}:5111/low_lo?freq={self.freq2}"
         response = urllib.request.urlopen(url)
         if response.status == 200:
-            print(f"Low LO frequency set to {self.lowlo}")
+            print(f"Low LO frequency set to {self.freq2}")
 
-    def set_rx1gain(self, rx1gain):
-        """Set RX 1 gain."""
-        self.rx1gain = rx1gain
-        url = f"http://{self.ip_address}:5111/gain?trx=rx&chan=0&v={self.rx1gain}"
+    def set_rx1gain(self, gain1):
+        self.gain1 = gain1
+        url = f"http://{self.ip_address}:5111/gain?trx=rx&chan=0&v={self.gain1}"
         response = urllib.request.urlopen(url)
         if response.status == 200:
-            print(f"RX1 gain set to {self.rx1gain}")
+            print(f"RX1 gain set to {self.gain1}")
 
-    def set_rx2gain(self, rx2gain):
+    def set_rx2gain(self, gain2):
         """Set RX 2 gain."""
-        self.rx2gain = rx2gain
-        url = f"http://{self.ip_address}:5111/gain?trx=rx&chan=1&v={self.rx2gain}"
+        self.gain2 = gain2
+        url = f"http://{self.ip_address}:5111/gain?trx=rx&chan=1&v={self.gain2}"
         response = urllib.request.urlopen(url)
         if response.status == 200:
-            print(f"RX2 gain set to {self.rx2gain}")
+            print(f"RX2 gain set to {self.gain2}")
 
-    def set_tx1gain(self, tx1gain):
+    def set_tx1gain(self, gain3):
         """Set TX 1 gain."""
-        self.tx1gain = tx1gain
-        url = f"http://{self.ip_address}:5111/gain?trx=tx&chan=0&v={self.tx1gain}"
+        self.gain3 = gain3
+        url = f"http://{self.ip_address}:5111/gain?trx=tx&chan=0&v={self.gain3}"
         response = urllib.request.urlopen(url)
         if response.status == 200:
-            print(f"TX1 gain set to {self.tx1gain}")
+            print(f"TX1 gain set to {self.gain3}")
 
-    def set_tx2gain(self, tx2gain):
+    def set_tx2gain(self, gain4):
         """Set TX 2 gain."""
-        self.tx2gain = tx2gain
-        url = f"http://{self.ip_address}:5111/gain?trx=tx&chan=1&v={self.tx2gain}"
+        self.gain4 = gain4
+        url = f"http://{self.ip_address}:5111/gain?trx=tx&chan=1&v={self.gain4}"
         response = urllib.request.urlopen(url)
         if response.status == 200:
-            print(f"TX2 gain set to {self.tx2gain}")
-
+            print(f"TX2 gain set to {self.gain4}")
 
       
